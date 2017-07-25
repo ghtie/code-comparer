4e5768ab5ade9795adf7647b4bc9247e510c37c2
Demo User <demouser@MacBook-Pro.local>
2017-07-25 11:29:42 -0400

    js panel

--- static/index.js
+++ static/index.js
@@ -11,22 +11,26 @@ function loadChecked() {
     $(".language-box").prop("checked", true);
 }
 
-function handleExample() {
-    $("#testappend").click(slidePanel);
-}
+$(document).ready(function(){
+    $("#testappend1").click(function(){
+        $("#panel2").slideToggle();
+    });
+  });
+  $(document).ready(function(){
+      $("#testappend").click(function(){
+          $("#panel").slideToggle();
+      });
+    });
+  //function handleExample() {
+    //$("#testappend").click();
+    //$("#panel").slideToggle("slow")
+  //}
+//function slidePanel() {
+  //  $("#panel").slideToggle("slow")
+    //$("#panel2").slideToggle("slow")
 
-function slidePanel() {
-    $("#panel").slideToggle("slow");
+//}
 
-}
-function handle() {
-    $("#testappend1").click(slide);
-}
-
-function slide() {
-    $("#panel2").slideToggle("slow");
-
-}
 
 
 //Toggle Functions
@@ -52,6 +56,3 @@ function setup() {
 }
 
 window.onload=loadChecked;
-
-
-//$(document).ready(function() {$('#test').append('<div class="flex-item4"><br>box 6<div>)'}
